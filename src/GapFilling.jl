523ca64705dac59ae99028f6f2da12496caab5d0
farhatlokmen <farhat.lokmen@gmail.com>
2023-03-17 19:00:19 +0300

    testing

--- src/GapFilling.jl
+++ src/GapFilling.jl
@@ -34,11 +34,11 @@ using XLSX
 
 
 export set_boundary, rename_resize_image, get_cloudP, get_path, plotHistogram, select_Img, getInfo, preprocess
-#generate_List, convert_to_offsets, selectUP_minNx, getZx, getZy, find_subset1, find_best_replicate, analyze,
+generate_List, convert_to_offsets, selectUP_minNx, getZx, getZy, find_subset1, find_best_replicate, analyze,
 #differenceP, postprocess
 
 include("preprocessing.jl")
-#include("modifiedDS.jl")
+include("modifiedDS.jl")
 #include("postprocessing.jl")
 
 end
